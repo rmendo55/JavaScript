87157df33dbed2fbc5d4594c26d9d07c1da71f4f
rmendo55 <mendozarafael914@gmail.com>
2021-01-20 22:22:14 -0800

    made a method for detection map

--- translateText/backEnd/custom-module/index.js
+++ translateText/backEnd/custom-module/index.js
@@ -5,33 +5,50 @@ let translate = require('translate');
 let detectLanguage = new DetectLanguage('38b946014b1f095853bff0e67a9f347b');
 
 /*
+Method to return list of detections 
+*/
+const getDetections = () => {
+    return new Promise((resolve, reject) => {
+        let codeMap = new Map();
+        detectLanguage.languages().then((result) => {
+            result.forEach(element => {
+                codeMap.set(`${element.code}`, {language: `${element.name}`});
+            });
+            if (codeMap.size === 0) {
+                reject('err');
+            }
+            else {
+                resolve(codeMap);
+            }
+        });
+    });
+};
+
+/*
 detectTextLanguase method will return two things from every entry of a map. The detected language and a value determining
 if the language is reliable
 */
 const detectTextLanguage = (text) => {
     return new Promise((resolve, reject) => {
-        detectLanguage.languages().then((result) => {
-            let codeMap = new Map();
-            result.forEach(element => {
-                codeMap.set(`${element.code}`, {
-                    language: `${element.name}`
-                });
-            });
-            if (codeMap.size === 0) {
-                reject('Error');
-            } else {
-                detectLanguage.detect(text).then((result) => {
-                    let languagesDetected = new Map();
-                    result.forEach(element => {
-                        element.forEach(current => {
-                            let code = codeMap.get(current.language);
-                            languagesDetected.set(code, {isReliable: `${current.isReliable}`, code: `${current.language}`});
+        getDetections().then((result) => {
+        let codeMap = result;
+        if (codeMap.size === 0) {
+            reject('err');
+        } else {
+            detectLanguage.detect(text).then((result) => {
+                let languagesDetected = new Map();
+                result.forEach(element => {
+                    element.forEach(current => {
+                        let code = codeMap.get(current.language);
+                        languagesDetected.set(code, {isReliable: `${current.isReliable}`, code: `${current.language}`});
                         });
                     });
                     resolve(languagesDetected);
                 });
-            }
-        });
+            }    
+        }).catch((err) => {
+            console.log(err);
+        }) ;
     });
 };
 
