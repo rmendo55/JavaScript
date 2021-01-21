25d4e5bc9204ac9854968fa4033ee9f4c14f7446
rmendo55 <mendozarafael914@gmail.com>
2021-01-20 19:57:38 -0800

    Detect all languages based on a text

---
+++ translateText/backEnd/custom-module/index.js
@@ -0,0 +1,45 @@
+//my api key: 38b946014b1f095853bff0e67a9f347b
+let DetectLanguage = require('detectlanguage');
+
+let detectLanguage = new DetectLanguage('38b946014b1f095853bff0e67a9f347b');
+
+const detectTextLanguage = (text) => {
+    return new Promise((resolve, reject) => {
+        detectLanguage.languages().then((result) => {
+            let codeMap = new Map();
+            result.forEach(element => {
+                codeMap.set(`${element.code}`, {
+                    language: `${element.name}`
+                });
+            });
+            if (codeMap.size === 0) {
+                reject('Error');
+            } else {
+                detectLanguage.detect(text).then((result) => {
+                    let languagesDetected = new Map();
+                    result.forEach(element => {
+                        element.forEach(current => {
+                            let code = codeMap.get(current.language);
+                            languagesDetected.set(code, {isReliable: `${current.isReliable}`});
+                        });
+                    });
+                    resolve(languagesDetected);
+                });
+            }
+        });
+    });
+};
+
+/* 
+Based on the API it is much faster when taking in 
+an array of text and returns a list of detections than doing
+a request for each text individually
+*/
+
+//Sample Run
+let text = ['My', 'name', 'is', 'Rafael', 'Mendoza'];
+detectTextLanguage(text).then((message) => {
+    console.log(message);
+}).catch((err) => {
+    console.log(`err: ${err}`)
+});
